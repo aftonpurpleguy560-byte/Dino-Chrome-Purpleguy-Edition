14665e16f4ac5e050cdbaf82759ef8055791800e
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-02-02 00:56:28 +0300

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,12 @@
+import DinoGame from './DinoGame' // DinoGame.jsx dosyanın src içinde olduğunu varsayıyorum
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-black">
+      <DinoGame />
+    </div>
+  )
+}
+
+export default App
+
