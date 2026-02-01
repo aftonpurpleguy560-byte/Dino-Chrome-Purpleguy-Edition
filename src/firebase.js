5b7fc2381e38d036b5d2e3f33c4ffd990b725b07
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-02-02 02:49:36 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAK61xyYFKoo19X0jDUIj378QMq2G13Ohs",
-  authDomain: "dino-chrome-purpleguy-edition.firebaseapp.com",
-  projectId: "dino-chrome-purpleguy-edition",
-  storageBucket: "dino-chrome-purpleguy-edition.firebasestorage.app",
-  messagingSenderId: "978217939718",
-  appId: "1:978217939718:web:00641130234a2db510a487"
+  apiKey: "AIzaSyCwdzsTXt8vFaWB7PaiJP6XObWcMsWZOEU",
+  authDomain: "purpledino-efe.firebaseapp.com",
+  projectId: "purpledino-efe",
+  storageBucket: "purpledino-efe.firebasestorage.app",
+  messagingSenderId: "98080359272",
+  appId: "1:98080359272:web:049aa594e37b756af30cdd"
 };
 
-// Firebase'i başlat
 const app = initializeApp(firebaseConfig);
-
-// Veritabanını (Firestore) dışa aktar ki DinoGame.jsx kullanabilsin
 export const db = getFirestore(app);
-
