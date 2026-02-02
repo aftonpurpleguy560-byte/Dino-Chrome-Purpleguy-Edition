3f0c213569d1634a0ac2431b9b45b07d4a90d45a
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-02-02 04:08:56 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwdzsTXt8vFaWB7PaiJP6XObWcMsWZOEU",
-  authDomain: "purpledino-efe.firebaseapp.com",
-  projectId: "purpledino-efe",
-  storageBucket: "purpledino-efe.firebasestorage.app",
-  messagingSenderId: "98080359272",
-  appId: "1:98080359272:web:049aa594e37b756af30cdd"
+  apiKey: "AIzaSyAAzJOlmCeigqjfPDAo68alda1QOVM4Cec",
+  authDomain: "purpleguy-chromedino-b69a7.firebaseapp.com",
+  projectId: "purpleguy-chromedino-b69a7",
+  storageBucket: "purpleguy-chromedino-b69a7.firebasestorage.app",
+  messagingSenderId: "697122629403",
+  appId: "1:697122629403:web:181cef564f7386448315be"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getFirestore(app); // Oyunun veritabanına bağlanması için bu şart
